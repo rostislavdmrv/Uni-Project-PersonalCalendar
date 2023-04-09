26fa235c82fd7126813422cb8c6126f0febaddb9
Rosty <roskko@abv.bg>
2023-04-09 22:25:20 +0300

    1

--- src/bg/tu_varna/sit/rostislav/Application.java
+++ src/bg/tu_varna/sit/rostislav/Application.java
@@ -1,4 +1,9 @@
 package bg.tu_varna.sit.rostislav;
 
 public class Application {
+
+    public static final String EXIT_COMMAND = "exit";
+
+    public static void main(String[] args) {
+    }
 }
---
+++ src/bg/tu_varna/sit/rostislav/menu/Menu.java
@@ -0,0 +1,8 @@
+package bg.tu_varna.sit.rostislav.menu;
+
+public interface Menu {
+     public void start();
+
+     public void printMenuHeader();
+
+}
